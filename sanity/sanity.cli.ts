24f5f19040980b1777713a82f822b53b696ae918
Your Name <Your Email>
2024-05-19 13:31:13 +0530

    removed sanity cli

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+// import {defineCliConfig} from 'sanity/cli'
 
-export default defineCliConfig({
-  api: {
-    projectId: '4bu2vq03',
-    dataset: 'production'
-  }
-})
+// export default defineCliConfig({
+//   api: {
+//     projectId: '4bu2vq03',
+//     dataset: 'production'
+//   }
+// })
